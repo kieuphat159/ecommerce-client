39065b0de2f2e700747e4a891f2ee5c8ad3b0762
kieuphat159 <kieuphat159@gmail.com>
2025-09-05 14:53:35 +0700

    UI for sign in

--- src/pages/auth/sign-in/Page.jsx
+++ src/pages/auth/sign-in/Page.jsx
@@ -1,9 +1,18 @@
 import SignInForm from "./components/SignInForm";
+import "./Page.css";
 
 function SignInPage() {
   return (
-    <div className="auth-page">
-      <SignInForm />
+    <div className="SignInPage">
+        <div className="body">
+            <div className="ImageSection">
+                <div className="Logo">3legant.</div>
+                <img src="./images/signup.jpg" alt="Sign In" />
+            </div>
+            <div className="FormContent">
+                <SignInForm />
+            </div>
+        </div>
     </div>
   );
 }
